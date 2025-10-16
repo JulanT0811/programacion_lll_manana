4f1ef0cfd774e6b7bea2f6168cdd498899354533
Julian Oswaldo Tixi Paez <jtlengua.9b@gmail.com>
2025-10-16 10:08:48 -0500

    CLASE 2025-10-16

---
+++ modulo2/javascript/02_variables.js
@@ -0,0 +1,36 @@
+console.log("variables");
+
+Saludo = "Hola utilizando variables";
+console.log("Variables Saludo", Saludo);
+console.log("Tipo de Variable", typeof (Saludo));
+
+numero = 256;
+console.log("Variables Numero", numero);
+console.log("Tipo de Variable", typeof (numero));
+
+Booleana = true;
+console.log("Variables Booleana", Booleana);
+console.log("Tipo de Variable", typeof (Booleana));
+
+indefinida = undefined;
+console.log("Variables Indefinida", indefinida);
+console.log("Tipo de Variable", typeof (indefinida));
+
+nula = null;
+console.log("Variables Nula", nula);
+console.log("Tipo de Variable", typeof (nula));
+
+bigentero = 8765675675785
+console.log("Variables BiIn", bigentero);
+console.log("Tipo de Variable", typeof (bigentero));
+
+persona = { 
+    nombre: "Juan", 
+    edad: 30
+}
+console.log("Variables Objeto", persona);
+console.log("Tipo de Variable", typeof (persona));
+
+numeros = [12,12,22,34,4,4];
+console.log("Variables Array", numeros);
+console.log("Tipo de Variable", typeof (numeros));
---
+++ modulo2/javascript/03_operadres_aridmeticos.js
@@ -0,0 +1,37 @@
+console.log("OPERADORES ARIDMETICOS");
+
+numero1 = 20;
+numero2 = 5;
+console.log("suema");
+suma = numero1 + numero2;
+console.log("Resultado", suma);
+console.log("resta");
+resta = numero1 - numero2;
+console.log("Resultado", resta);
+console.log("multiplicacion");
+multiplicacion = numero1 * numero2;
+console.log("Resultado", multiplicacion);
+console.log("division");
+division = numero1 / numero2;
+console.log("Resultado", division);
+console.log("modulo");
+modulo = numero1 % numero2;
+console.log("Resultado", modulo);
+console.log("potencia");
+exponente = numero1 ** numero2;
+console.log("Resultado", potencia);
+
+console.log("Operadores Comparacion");
+console.log("Igualdad debil ==", 5 == 5);
+console.log("Igualdad Estricta ===", "5"===5);
+console.log("Desigualdad Debil !=", "5"!= 5);
+console.log("Desigualdad Estricta !==", "5"!== 5);
+console.log("Mayor que >", 5 > 5);
+console.log("Menor que <", 5 < 5);
+console.log("Mayor o Igual que >=", "5" >= 5);
+console.log("Menor o Igual que <=", "5" <= 5);
+
+console.log("Operadores Logicos");
+console.log("y And &&", "5"<=5 && "5">=5);
+console.log("o Or ||", "5"<=5 || "6">=4);
+console.log("Negacion !", !(5==5));
