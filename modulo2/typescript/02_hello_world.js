74636005ffe3993bd1e11a6cb588e0907f37e37e
JulanT0811 <jtlengua.9b@gmail.com>
2025-10-31 08:06:27 -0500

    2025-10-30

---
+++ modulo2/typescript/02_hello_world.js
@@ -0,0 +1,11 @@
+var edad = 30;
+var nombre = 'Maria';
+var activo = true;
+var valores = 6;
+if (edad > 18 && activo) {
+    console.log('trabajo activo');
+}
+else {
+    console.log('No trabaja');
+}
+var frutas;
---
+++ modulo2/typescript/03_funciones.js
@@ -0,0 +1,10 @@
+//funcion basica
+function saludar(nombre) {
+    return "Hola ".concat(nombre);
+}
+console.log(saludar('Francisco'));
+//funcion flecha
+var sumar = function (a, b) {
+    return a + b;
+};
+console.log(sumar(5, 5));
